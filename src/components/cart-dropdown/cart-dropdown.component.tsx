1dd69ed1bd8f4a88fe2240507407b85841b15706
Devon McKenzie <novithian@gmail.com>
2022-08-15 17:05:31 -0400

    Started making the app mobile-friendly

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
@@ -11,9 +12,9 @@ const CartDropdown = () => {
 	const navigate = useNavigate();
 	const cartItems = useSelector(selectCartItems);
 
-	const moveToCheckout = () => {
+	const moveToCheckout = useCallback(() => {
 		navigate("checkout");
-	};
+	}, [navigate]);
 
 	return (
 		<div className="cart-dropdown-container">
